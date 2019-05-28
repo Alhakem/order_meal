f5369ead49b4ff8d20cbebe7838e04f70b76d66c
YaserAlnajjar <kasseryasser@windowslive.com>
2019-05-28 05:00:05 +0300

    Update src files with latest (from Drive)

--- src/meal_list/urls.py
+++ src/meal_list/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import home, meal_detail,add_meal,order_form
+from . import views 
+
+
 urlpatterns = [
-    path("", home, name='home'),
-    path("detail/<int:post_id>/", meal_detail, name='details'),
-    path('add_meal', add_meal, name='add_meals'),
-    path('detail/order_Form', order_form, name='order_Forms'),
+    path("", views.home, name='home'),
+    path("detail/<int:meal_id>/", views.meal_detail, name='details'),
+    path('add_meal', views.add_meal, name='add_meals'),
+    path('<int:meal_id>/update$', views.update_meal, name='edit_meals'),
+    path('^detail/<int:meal_id>/order_Form$',views.order_form, name='order_Forms'),
+    path("list_order", views.list_order, name='list_order'),
+    path("success-order", views.success_order, name='success-order'),
+     
 ]
